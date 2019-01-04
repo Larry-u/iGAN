6c01a511aae9beeae2e5ab9693706af847afcb7b
Toby the potato <tobyclh@gmail.com>
2019-01-04 15:50:35 +0900

    speed up line extraction

--- lib/HOGNet_pytorch.py
+++ lib/HOGNet_pytorch.py
@@ -14,6 +14,7 @@ class HOGNet():
         self.BS = BS
         self.nc = nc
         self.use_bin = use_bin
+        self.G_f = None
 
     def _comp_mask(self, mask):
         BS = self.BS
@@ -29,57 +30,55 @@ class HOGNet():
         masks = torch.zeros_like(_masks)
         masks[_masks > 1e-5] = 1
         return masks
+        
     def get_hog(self, x_o):
         if isinstance(x_o, np.ndarray):
             x_o = torch.Tensor(x_o)
         if isinstance(x_o, torch.Tensor):
             x_o = x_o.cuda()
-        use_bin = self.use_bin
-        NO = self.NO
-        BS = self.BS
-        nc = self.nc
+
         x = (x_o + 1) / 2
-        Gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]) / 4.0
-        Gy = Gx.T
-        f1_w = []
-        for i in range(NO):
-            t = np.pi / NO * i
-            g = np.cos(t) * Gx + np.sin(t) * Gy
-            gg = np.tile(g[np.newaxis, np.newaxis, :, :], [1, 1, 1, 1])
-            f1_w.append(gg)
-        f1_w = np.concatenate(f1_w, axis=0)
-        G = np.concatenate([Gx[np.newaxis, np.newaxis, :, :], Gy[np.newaxis, np.newaxis, :, :]], axis=0)
-        G_f = torch.Tensor(G).cuda()
+        if self.G_f is None:
+            Gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]) / 4.0
+            Gy = Gx.T
+            f1_w = []
+            for i in range(self.NO):
+                t = np.pi / self.NO * i
+                g = np.cos(t) * Gx + np.sin(t) * Gy
+                gg = np.tile(g[np.newaxis, np.newaxis, :, :], [1, 1, 1, 1])
+                f1_w.append(gg)
+            f1_w = np.concatenate(f1_w, axis=0)
+            G = np.concatenate([Gx[np.newaxis, np.newaxis, :, :], Gy[np.newaxis, np.newaxis, :, :]], axis=0)
+            self.G_f = torch.Tensor(G).cuda()
+            a = np.cos(np.pi / self.NO)
+            l1 = 1 / (1 - a)
+            l2 = a / (1 - a)
+            eps = 1e-3
+            if self.nc == 3:
+                x_gray = x.mean(1).unsqueeze(1)
+            else:
+                x_gray = x
+            self.f1 = torch.Tensor(f1_w).cuda()
+        h0 = F.conv2d(x_gray, self.f1, padding=(1,1)).abs()
+        g = F.conv2d(x_gray, self.G_f, padding=(1,1))
 
-        a = np.cos(np.pi / NO)
-        l1 = 1 / (1 - a)
-        l2 = a / (1 - a)
-        eps = 1e-3
-        if nc == 3:
-            x_gray = x.mean(1).unsqueeze(1)
-        else:
-            x_gray = x
-        f1 = torch.Tensor(f1_w).cuda()
-        h0 = F.conv2d(x_gray, f1, padding=(1,1)).abs()
-        g = F.conv2d(x_gray, G_f, padding=(1,1))
-
-        if use_bin:
+        if self.use_bin:
             gx = g[:, [0], ...]
             gy = g[:, [1], ...]
             gg = torch.sqrt(gx * gx + gy * gy + eps)
             hk = torch.Tensor([l1]).cuda() * h0 - torch.Tensor([l2]).cuda() * gg
             hk[hk <= 0] = 0
 
-            bf_w = torch.zeros((NO, NO, 2 * BS, 2 * BS))
-            b = 1 - np.abs((np.arange(1, 2 * BS + 1) - (2 * BS + 1.0) / 2.0) / BS)
+            bf_w = torch.zeros((self.NO, self.NO, 2 * self.BS, 2 * self.BS))
+            b = 1 - np.abs((np.arange(1, 2 * self.BS + 1) - (2 * self.BS + 1.0) / 2.0) / self.BS)
             b = b[np.newaxis, :]
             bb = b.T.dot(b)
             bb = torch.Tensor(bb)
-            for n in range(NO):
+            for n in range(self.NO):
                 bf_w[n, n] = bb
 
             bf = bf_w.cuda()
-            h_f = F.conv2d(hk, bf, padding=(BS // 2, BS // 2), stride=BS)
+            h_f = F.conv2d(hk, bf, padding=(self.BS // 2, self.BS // 2), stride=self.BS)
             return h_f
         else:
             return g
