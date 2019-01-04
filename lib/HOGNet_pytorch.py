2bcfe3f91d4118b2235e4367502dc708621d86a8
Toby the potato <tobyclh@gmail.com>
2019-01-04 15:55:53 +0900

    further speed up line extraction

--- lib/HOGNet_pytorch.py
+++ lib/HOGNet_pytorch.py
@@ -14,7 +14,7 @@ class HOGNet():
         self.BS = BS
         self.nc = nc
         self.use_bin = use_bin
-        self.G_f = None
+        self.G_f = self.f1 = self.bf = None
 
     def _comp_mask(self, mask):
         BS = self.BS
@@ -30,7 +30,7 @@ class HOGNet():
         masks = torch.zeros_like(_masks)
         masks[_masks > 1e-5] = 1
         return masks
-        
+
     def get_hog(self, x_o):
         if isinstance(x_o, np.ndarray):
             x_o = torch.Tensor(x_o)
@@ -51,8 +51,8 @@ class HOGNet():
             G = np.concatenate([Gx[np.newaxis, np.newaxis, :, :], Gy[np.newaxis, np.newaxis, :, :]], axis=0)
             self.G_f = torch.Tensor(G).cuda()
             a = np.cos(np.pi / self.NO)
-            l1 = 1 / (1 - a)
-            l2 = a / (1 - a)
+            self.l1 = torch.Tensor([1 / (1 - a)]).cuda()
+            self.l2 = torch.Tensor([a / (1 - a)]).cuda()
             eps = 1e-3
             if self.nc == 3:
                 x_gray = x.mean(1).unsqueeze(1)
@@ -66,19 +66,19 @@ class HOGNet():
             gx = g[:, [0], ...]
             gy = g[:, [1], ...]
             gg = torch.sqrt(gx * gx + gy * gy + eps)
-            hk = torch.Tensor([l1]).cuda() * h0 - torch.Tensor([l2]).cuda() * gg
+            hk = self.l1 * h0 - self.l2 * gg
             hk[hk <= 0] = 0
 
-            bf_w = torch.zeros((self.NO, self.NO, 2 * self.BS, 2 * self.BS))
-            b = 1 - np.abs((np.arange(1, 2 * self.BS + 1) - (2 * self.BS + 1.0) / 2.0) / self.BS)
-            b = b[np.newaxis, :]
-            bb = b.T.dot(b)
-            bb = torch.Tensor(bb)
-            for n in range(self.NO):
-                bf_w[n, n] = bb
-
-            bf = bf_w.cuda()
-            h_f = F.conv2d(hk, bf, padding=(self.BS // 2, self.BS // 2), stride=self.BS)
+            if self.bf is None:
+                bf_w = torch.zeros((self.NO, self.NO, 2 * self.BS, 2 * self.BS))
+                b = 1 - np.abs((np.arange(1, 2 * self.BS + 1) - (2 * self.BS + 1.0) / 2.0) / self.BS)
+                b = b[np.newaxis, :]
+                bb = b.T.dot(b)
+                bb = torch.Tensor(bb)
+                for n in range(self.NO):
+                    bf_w[n, n] = bb
+                self.bf = bf_w.cuda()
+            h_f = F.conv2d(hk, self.bf, padding=(self.BS // 2, self.BS // 2), stride=self.BS)
             return h_f
         else:
             return g
